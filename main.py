502d2d09bbde315e68a21fdb5b9ae443dfc308e0
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2025-01-18 19:48:32 +0800

    add metadata handler

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psycopg
 import signal
 import sys
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -51,7 +52,7 @@ def create_enums(db: Database) -> None:
         DO $$
         BEGIN
             IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'car_type') THEN
-                CREATE TYPE car_type AS ENUM ('RWD', 'AWD');
+                CREATE TYPE car_type AS ENUM ('RWD', 'AWD', 'UNKNOWN');
             END IF;
         END
         $$;
@@ -64,7 +65,7 @@ def create_enums(db: Database) -> None:
         DO $$
         BEGIN
             IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'car_colour') THEN
-                CREATE TYPE car_colour AS ENUM ('Pearl White', 'Solid Black', 'Deep Blue Metallic', 'Stealth Grey', 'Quicksilver', 'Ultra Red');
+                CREATE TYPE car_colour AS ENUM ('Pearl White', 'Solid Black', 'Deep Blue Metallic', 'Stealth Grey', 'Quicksilver', 'Ultra Red', 'UNKNOWN');
             END IF;
         END
         $$;
@@ -77,7 +78,7 @@ def create_enums(db: Database) -> None:
         DO $$
         BEGIN
             IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'car_wheels') THEN
-                CREATE TYPE car_wheels AS ENUM ('18" Photon Wheels', '19" Nova Wheels');
+                CREATE TYPE car_wheels AS ENUM ('18" Photon Wheels', '19" Nova Wheels', 'UNKNOWN');
             END IF;
         END
         $$;
@@ -90,7 +91,7 @@ def create_enums(db: Database) -> None:
         DO $$
         BEGIN
             IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'car_interior') THEN
-                CREATE TYPE car_interior AS ENUM ('Black', 'White');
+                CREATE TYPE car_interior AS ENUM ('Black', 'White', 'UNKNOWN');
             END IF;
         END
         $$;
@@ -199,6 +200,24 @@ def insert_car(db: Database, car_id: str) -> None:
     )
 
 
+def insert_car_metadata(
+    db: Database,
+    car_id: str,
+    car_type: str,
+    car_colour: str,
+    car_wheels: str,
+    car_interior: str,
+) -> None:
+    db.execute(
+        """
+        INSERT INTO car_metadata (car_id, type, colour, wheels, interior)
+        VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (car_id) DO NOTHING
+        """,
+        (car_id, car_type, car_colour, car_wheels, car_interior),
+    )
+
+
 def insert_car_location(db: Database, car_id: str, location_name: str) -> None:
     location_id = get_location_id(db, location_name)
     db.execute(
@@ -209,6 +228,22 @@ def insert_car_location(db: Database, car_id: str, location_name: str) -> None:
         """,
         (car_id, location_id),
     )
+
+
+def determine_car_colour(car_html: str) -> str:
+    if re.search(r"Pearl White", car_html):
+        return "Pearl White"
+    elif re.search(r"Solid Black", car_html):
+        return "Solid Black"
+    elif re.search(r"Deep Blue Metallic", car_html):
+        return "Deep Blue Metallic"
+    elif re.search(r"Stealth Grey", car_html):
+        return "Stealth Grey"
+    elif re.search(r"Quicksilver", car_html):
+        return "Quicksilver"
+    elif re.search(r"Ultra Red", car_html):
+        return "Ultra Red"
+    return "UNKNOWN"
 
 
 def scrape_website_data(db: Database) -> None:
@@ -226,9 +261,24 @@ def scrape_website_data(db: Database) -> None:
 
             articles = driver.find_elements(By.CSS_SELECTOR, "article.result.card")
             for article in articles:
+                article_html = article.get_attribute("innerHTML")
                 car_id = article.get_attribute("data-id")
+                car_type = (
+                    "AWD" if bool(re.search(r"All-Wheel"), article_html) else "RWD"
+                )
+                car_colour = determine_car_colour(article_html)
+                car_wheels = (
+                    '18" Photon Wheels'
+                    if bool(re.search(r"Photon Wheels"), article_html)
+                    else '19" Nova Wheels'
+                )
+                car_interior = (
+                    "Black"
+                    if bool(re.search(r"Black Premium Interior"), article_html)
+                    else "White"
+                )
                 insert_car(db, car_id)
-                # insert_car_metadata(db, car_id, type, colour, wheels, interior)
+                insert_car_metadata(db, car_id, car_type, car_colour, car_wheels, car_interior)
                 insert_car_location(db, car_id, state)
 
             driver.quit()
