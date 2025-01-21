1fdd4f1b2bba1e431dfaa43901f7e1e2963097da
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2025-01-21 12:41:01 +0800

    partial refactor to use inventory api

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,7 @@
-import re
+import math
+import json
+import urllib.parse
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from time import sleep
 from datetime import datetime
 from database import Database
@@ -11,61 +12,61 @@ from models import (
     get_active_cars,
     mark_car_as_removed,
 )
-from .helpers import determine_car_colour
 
 
 def scrape_website_data(db: Database) -> None:
     active_cars = set(get_active_cars(db))
     found_cars = set()
+    offset = 0
+    query_parameters = {
+        "query": {
+            "model": "m3",
+            "condition": ["used", "new"],
+            "arrangeby": "Price",
+            "order": "asc",
+            "market": "AU",
+            "language": "en",
+            "super_region": "asia pacific",
+            "lng": 151.21,
+            "lat": -33.868,
+            "zip": "2000",
+            "range": 9999,
+        },
+        "offset": offset,
+        "count": 50,
+        "outsideOffset": 0,
+        "outsideSearch": False,
+    }
+    encoded_query = urllib.parse.urlencode({"query": json.dumps(query_parameters)})
+    url = f"https://www.tesla.com/inventory/api/v4/inventory-results?{encoded_query}"
     try:
         driver = webdriver.Chrome()
-        for state in ["ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"]:
-            url = f"https://www.tesla.com/en_AU/inventory/new/m3?RegistrationProvince={state}"
-            driver.get(url)
-            sleep(5)
+        driver.get(url)
+        page_source = driver.find_element("tag name", "pre").text
+        data = json.loads(page_source)
+        scraped_pages = 1
+        total_pages_to_scrape = math.ceil(data["total_matches_found"] / 50)
 
-            confirm_button = driver.find_element(By.XPATH, "//button[text()='Confirm']")
-            confirm_button.click()
-            sleep(5)
-
-            articles = driver.find_elements(By.CSS_SELECTOR, "article.result.card")
-            for article in articles:
-                article_html = article.get_attribute("innerHTML")
-                car_id = article.get_attribute("data-id")
-                car_type = "AWD" if re.search(r"All-Wheel", article_html) else "RWD"
-                car_colour = determine_car_colour(article_html)
-                car_wheels = (
-                    '18" Photon Wheels'
-                    if re.search(r"Photon Wheels", article_html)
-                    else '19" Nova Wheels'
-                )
-                car_interior = (
-                    "Black"
-                    if re.search(r"Black Premium Interior", article_html)
-                    else "White"
-                )
-                car_price = int(
-                    re.sub(
-                        r"\D",
-                        "",
-                        article.find_element(
-                            By.CSS_SELECTOR, "span.result-purchase-price"
-                        ).text,
-                    )
-                )
-
-                insert_car(db, car_id)
-                insert_car_metadata(
-                    db,
-                    car_id,
-                    car_type,
-                    car_colour,
-                    car_wheels,
-                    car_interior,
-                    car_price,
-                )
-                insert_car_location(db, car_id, state)
-                found_cars.add(car_id)
+        for entry in data["results"]:
+            car_id = entry["VIN"]
+            car_type = entry["TRIM"]
+            car_colour = entry["PAINT"]
+            car_wheels = entry["WHEELS"]
+            car_interior = entry["INTERIOR"][0]
+            car_price = entry["TotalPrice"]
+            car_state = entry["StateProvince"]
+            insert_car(db, car_id)
+            insert_car_metadata(
+                db,
+                car_id,
+                car_type,
+                car_colour,
+                car_wheels,
+                car_interior,
+                car_price,
+            )
+            insert_car_location(db, car_id, car_state)
+            found_cars.add(car_id)
 
         driver.quit()
         removed_cars = active_cars - found_cars
